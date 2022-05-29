cab291f98610868acdd631e956581d03441163ca
KarthikBenki <karthikmc007@gmail.com>
2022-05-29 20:22:09 +0530

    Added addressbook backend and frontend project to do crud operations

---
+++ addressbookapp_react/src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+import Form from './components/Form';
+import Home from './components/Home';
+import { BrowserRouter,Route,Switch } from 'react-router-dom';
+
+function App() {
+
+  return (
+    <div className="App">
+       <BrowserRouter>
+       <Switch>
+         <Route exact path='/form' component={Form}></Route>
+         <Route path='/'  component={Home}></Route>
+         <Route path='/home' component={Home}></Route>
+         {/* <Route exact path="/form/:id" component={Form}></Route> */}
+       </Switch>
+    </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
